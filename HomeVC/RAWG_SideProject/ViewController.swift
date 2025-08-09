12266c1e4666c75f5aacb16f6a73c2226e1809a2
ToolManHsu <willy548798@gmail.com>
2025-08-09 22:19:20 +0800

    build: MainTabBar

---
+++ HomeVC/RAWG_SideProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RAWG_SideProject
+//
+//  Created by Willy Hsu on 2025/8/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabBarController/MainTabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabBarController.swift
+//  RAWG_SideProject
+//
+//  Created by Willy Hsu on 2025/8/9.
+//
+
+import Foundation
