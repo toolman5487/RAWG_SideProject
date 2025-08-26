06e947a60b49e5bb1375f8533852bd45597170ca
Willy <willy548798@gmail.com>
2025-08-26 21:55:47 +0800

    feat: build HomeVC SearchBar

--- Home/ViewController/HomeViewController.swift
+++ Home/ViewController/HomeViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Willy Hsu on 2025/8/9.
 //
-
 import Foundation
 import UIKit
 import SnapKit
@@ -13,14 +12,25 @@ import Combine
 class HomeViewController: UIViewController {
 
 // MARK: - UI
-    
+    private lazy var searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: nil)
+        controller.obscuresBackgroundDuringPresentation = false
+        controller.searchBar.placeholder = "Search Games ..."
+        return controller
+    }()
     
 // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setupNavigation()
+    }
+    
+    private func setupNavigation() {
         self.title = "RAWG"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .always
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
 }
