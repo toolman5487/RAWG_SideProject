9c32c3110930891f051049a128af57cb3363bdcf
Willy <willy548798@gmail.com>
2025-09-28 23:13:27 +0800

    feat: build NGListCell

--- NewGamePage/View/NewGameViewCell.swift
+++ NewGamePage/View/NewGameViewCell.swift
@@ -13,6 +13,8 @@ protocol NGgenreListCellDelegate: AnyObject {
     func didSelectGenre(_ genre: GameGenreModel)
 }
 
+// MARK: - NewGame GenreList
+
 class NGgenreListCell: UITableViewCell {
     
     weak var delegate: NGgenreListCellDelegate?
@@ -90,4 +92,87 @@ extension NGgenreListCell: UICollectionViewDataSource, UICollectionViewDelegate 
     }
 }
 
+// MARK: - NewGame Collection
 
+protocol NGgameListCellDelegate: AnyObject {
+    func didSelectGame(_ game: GameListItemModel)
+}
+
+class NGgameListCell: UITableViewCell {
+    
+    weak var delegate: NGgameListCellDelegate?
+    private var games: [GameListItemModel] = []
+    private var heightConstraint: Constraint?
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 12
+        layout.minimumLineSpacing = 16
+        layout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(NewGameListCell.self, forCellWithReuseIdentifier: "NewGameListCell")
+        return collectionView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        backgroundColor = .clear
+        selectionStyle = .none
+        
+        contentView.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            heightConstraint = make.height.equalTo(100).constraint
+        }
+    }
+    
+    func configure(with games: [GameListItemModel]) {
+        self.games = games
+        collectionView.reloadData()
+        
+        let rows = (games.count + 2) / 3
+        let cellHeight: CGFloat = 160
+        let spacing: CGFloat = 16
+        let totalHeight = CGFloat(rows) * cellHeight + CGFloat(rows - 1) * spacing + 32
+        
+        heightConstraint?.update(offset: totalHeight)
+    }
+}
+
+extension NGgameListCell: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return games.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewGameListCell", for: indexPath) as! NewGameListCell
+        cell.configure(with: games[indexPath.item])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        let selectedGame = games[indexPath.item]
+        delegate?.didSelectGame(selectedGame)
+    }
+}
+
+extension NGgameListCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.frame.width - 56) / 3
+        return CGSize(width: width, height: 160)
+    }
+} 
